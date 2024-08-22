9ca9c1b71cf636021a85739edc4d47b3732f6af0
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-22 13:07:10 +0100

    Lint 01

--- 0x00-ES6_basic/eslint.config.js
+++ 0x00-ES6_basic/eslint.config.js
@@ -1,8 +1,57 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import airbnbBase from 'eslint-config-airbnb-base';
+import pluginJest from 'eslint-plugin-jest';
 
+const eslintConfig = {
+  languageOptions: {
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      jest: 'readonly',
+      // browser: false,
+      es6: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+  },
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    // Add rules from airbnb-base and plugin:jest/all if needed
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
+};
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: airbnbBase.globals,
+      parserOptions: airbnbBase.parserOptions,
+    },
+    rules: {
+      ...airbnbBase.rules,
+      ...eslintConfig.rules,
+    },
+  },
+  {
+    languageOptions: {
+      globals: pluginJest.configs.all.globals,
+    },
+    rules: {
+      ...pluginJest.configs.all.rules,
+    },
+  },
 ];
