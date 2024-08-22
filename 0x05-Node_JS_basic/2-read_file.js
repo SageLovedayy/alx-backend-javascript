d5ece4344ceb4fc31f60b427fa9001d962aabdb4
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-22 11:46:38 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw Error('Cannot load the database');
   }
@@ -14,7 +14,7 @@ function countStudents (path) {
       firstName: student[0],
       lastName: student[1],
       age: student[2],
-      field: student[3]
+      field: student[3],
     }));
 
   console.log(`Number of students: ${studentsArray.length}`);
@@ -22,13 +22,13 @@ function countStudents (path) {
   const fields = [...new Set(studentsArray.map((student) => student.field))];
   fields.forEach((field) => {
     const studentsInField = studentsArray.filter(
-      (student) => student.field === field
+      (student) => student.field === field,
     );
     const studentsByFirstName = studentsInField
       .map((student) => student.firstName)
       .join(', ');
     console.log(
-      `Number of students in ${field}: ${studentsInField.length}. List: ${studentsByFirstName}`
+      `Number of students in ${field}: ${studentsInField.length}. List: ${studentsByFirstName}`,
     );
   });
 }
