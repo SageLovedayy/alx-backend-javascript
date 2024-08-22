1d6e9f3e7d0275342faf67f92a280a9489eab047
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-22 12:15:49 +0100

    read file async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,40 @@
 const fs = require('fs');
 
-async function countStudents (path) {
+async function countStudents(path) {
   let data;
+
   try {
     data = await fs.promises.readFile(path, 'utf8');
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
   }
-  const students = data.split('\n')
+
+  const studentsArray = data
+    .split('\n')
     .map((student) => student.split(','))
-    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .slice(1)
+    .filter((student) => student.length === 4)
     .map((student) => ({
       firstName: student[0],
       lastName: student[1],
       age: student[2],
-      field: student[3]
+      field: student[3],
     }));
-  const csStudents = students
-    .filter((student) => student.field === 'CS')
-    .map((student) => student.firstName);
-  const sweStudents = students
-    .filter((student) => student.field === 'SWE')
-    .map((student) => student.firstName);
-  console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  return { students, csStudents, sweStudents };
+
+  console.log(`Number of students: ${studentsArray.length}`);
+
+  const fields = [...new Set(studentsArray.map((student) => student.field))];
+  fields.forEach((field) => {
+    const studentsInField = studentsArray.filter(
+      (student) => student.field === field,
+    );
+    const studentsByFirstName = studentsInField
+      .map((student) => student.firstName)
+      .join(', ');
+    console.log(
+      `Number of students in ${field}: ${studentsInField.length}. List: ${studentsByFirstName}`,
+    );
+  });
 }
 
 module.exports = countStudents;
