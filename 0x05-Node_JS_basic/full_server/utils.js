378f8390554d62ad0ff7c78aa19c3240d2e9525e
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-23 23:36:35 +0100

    lint full_server/utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 
-async function countStudents (path) {
+async function countStudents(path) {
   let data;
   try {
     data = await fs.promises.readFile(path, 'utf8');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-  const students = data.split('\r\n').slice(1)
+  const students = data
+    .split('\r\n')
+    .slice(1)
     .map((student) => student.split(','))
     .map((student) => ({
       firstName: student[0],
       lastName: student[1],
       age: student[2],
-      field: student[3]
+      field: student[3],
     }));
-  const fields = students.map(student => student.field);
+  const fields = students.map((student) => student.field);
   const uniqueFields = new Set(fields);
   const studentsByField = {};
   for (const field of uniqueFields) {
